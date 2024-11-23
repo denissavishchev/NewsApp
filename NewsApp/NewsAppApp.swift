21a756cdaf2c0194df943ac84fc1393ba8eee020
Denis Savishchev <denis.savishchev@gmail.com>
2024-11-23 12:44:41 +0100

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Devis on 23/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
